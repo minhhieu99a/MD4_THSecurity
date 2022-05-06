e1dce0c8a3e5634c08d0e65f374e33ab1682405a
minhhieu99a <hieunb99@gmail.com>
2022-05-06 15:26:39 +0700

    done

---
+++ src/main/java/com/codegym/demospringsecurity/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.codegym.demospringsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(userBuilder.username("Hieu").password("123456").roles("ADMIN").build());
+        manager.createUser(userBuilder.username("Linh").password("123456").roles("USER").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/home").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/user/**").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+        http.csrf().disable();
+    }
+}
---
+++ src/main/java/com/codegym/demospringsecurity/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.codegym.demospringsecurity.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
